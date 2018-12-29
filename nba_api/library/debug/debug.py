5c0cef9e38eae771ab9d0b8d085b283fe7fed960
Avik Dureja <adurejabackup@gmail.com>
2018-12-29 12:43:10 -0500

    added beautifulsoup for proxy selection

--- nba_api/library/debug/debug.py
+++ nba_api/library/debug/debug.py
@@ -1,9 +1,8 @@
 import os
-from lxml.html import fromstring
+from bs4 import BeautifulSoup
+import html5lib
 import random
 import requests
-from itertools import cycle
-import traceback
 DEBUG = False
 
 # Saving and loading responses from text files so that you do not have to do multiple requests when debugging.
@@ -12,12 +11,18 @@ DEBUG_STORAGE = False
 def get_proxies():
     url = 'https://free-proxy-list.net/'
     response = requests.get(url)
-    parser = fromstring(response.text)
-    proxies = set()
-    for i in parser.xpath('//tbody/tr')[:19]:
-        if i.xpath('.//td[7][contains(text(),"yes")]'):
-            proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
-            proxies.add(proxy)
+    soup = BeautifulSoup(response.content, 'html5lib')
+    # proxies = set()
+    proxy_rows = [i.find_parent('tr') for i in soup(text=re.compile(r'yes'))]
+    proxies = set([':'.join([x.getText() for x in i.findAll('td')[:2]]) for i in proxy_rows])
+    # for i in parser.xpath('//tbody/tr')[:19]:
+    #     if i.xpath('.//td[7][contains(text(),"yes")]'):
+    #         proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
+    #         proxies.add(proxy)
+    #         print proxy
+    # for p in proxy_rows:
+    # 	proxy = ""
+    print(proxies)
     return proxies
 
 
